b890a4d6ffcb8e52e27e4748383fa3e4e722ee04
Navinma <136572618+Navinma@users.noreply.github.com>
2023-07-11 22:45:51 +0530

    Add files via upload

---
+++ 11-07-23_Lab-09/Lab_09.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<?php
+$x=6;
+$y=4;
+
+echo "Sum =".($x + $y)."<br>";
+echo "Sub =".($x - $y)."<br>";
+echo "Mul =".($x * $y)."<br>";
+echo "Div =".($x / $y)."<br>";
+?>
+</body>
+</html>
